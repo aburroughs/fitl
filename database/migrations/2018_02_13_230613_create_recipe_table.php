5c9c5b0ca4e2b904d382b6127aee2119fc3b03fb
Adam Burroughs <adam.burroughs01@gmail.com>
2018-02-13 16:19:25 -0700

    added a recipe database migration file

---
+++ database/migrations/2018_02_13_230613_create_recipe_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('ingredients');
+            $table->string('cook_time');
+            $table->text('instructions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recipes');
+    }
+}
